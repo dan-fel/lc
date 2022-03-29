c01c0d906b23407fbdb337cf7ab8a6986e638286
danfel <daniel.felczak@gmail.com>
2022-03-30 00:00:33 +0200

    Add insertion to bst

--- ds/binary_tree.hpp
+++ ds/binary_tree.hpp
@@ -1,28 +1,87 @@
 #include <cstdint>
+#include <iostream>
 
 class Node
 {
 public:
-    Node(const Node &left, const Node &right, const std::int32_t val) noexcept : left_{left}, right_{right}, val_{val} {}
-    Node(Node *parent, const Node &left, const Node &right, const std::int32_t val) noexcept : p_{parent}, left_{left}, right_{right}, val_{val} {}
+    Node(Node *left, Node *right, const std::int32_t val) noexcept : p_{nullptr}, left_{left}, right_{right}, val_{val} {}
+    Node(Node *parent, Node *left, Node *right, const std::int32_t val) noexcept : p_{parent}, left_{left}, right_{right}, val_{val} {}
+
+    Node *p_;
+    Node *left_;
+    Node *right_;
+    const std::int32_t val_;
 
 private:
-    Node *p_{nullptr};
-    const Node &left_;
-    const Node &right_;
-    const std::int32_t val_;
 };
 
 class BinaryTree
 {
 public:
-    BinaryTree(const Node &root) noexcept : root_{root} {}
+    BinaryTree(Node *root) noexcept : root_{root} {}
 
-    void Find(std::int32_t key) {}
-    void Insert(std::int32_t key) {}
+    Node *CreateNode(Node *parent, std::int32_t val)
+    {
+        return new Node(parent, nullptr, nullptr, val);
+    }
+    void DestroyNode()
+    {
+    }
+    void Find(std::int32_t val) {}
+    void Insert(std::int32_t val)
+    {
+        // Use non-recursive iteration.
+
+        bool left{false};
+        bool right{false};
+
+        Node *current{root_};
+        Node *prev{root_};
+
+        while (current != nullptr)
+        {
+            left = false;
+            right = false;
+            if (current->val_ <= val)
+            {
+
+                prev = current;
+                current = current->right_;
+                right = true;
+            }
+            else
+            {
+
+                prev = current;
+                current = current->left_;
+                left = true;
+            }
+        }
+
+        if (right)
+        {
+            prev->right_ = CreateNode(prev, val);
+        }
+        else
+        {
+            prev->left_ = CreateNode(prev, val);
+        }
+    }
     void Remove(std::int32_t key) {}
-    void Free() {}
+    void PrintTree(Node *node)
+    {
+        // Print recursively
+
+        if (node == nullptr)
+        {
+            return;
+        }
+
+        std::cout << node->val_ << std::endl;
+        PrintTree(node->left_);
+        PrintTree(node->right_);
+    }
 
 private:
-    const Node &root_;
+    Node *root_;
 };
--- ds/main.cpp
+++ ds/main.cpp
@@ -4,5 +4,18 @@
 
 int main()
 {
+    Node *root{new Node(nullptr, nullptr, nullptr, 5)};
+
+    BinaryTree bst{root};
+
+    bst.Insert(10);
+    bst.Insert(11);
+    bst.Insert(12);
+
+    bst.Insert(1);
+    bst.Insert(2);
+    bst.Insert(3);
+    bst.PrintTree(root);
+
     return 0;
 }
