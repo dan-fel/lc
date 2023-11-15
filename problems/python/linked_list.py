6e22b7bbfa6c5199ea7be9b9f12c86d78c17215c
Daniel Felczak <daniel.felczak@walley.se>
2023-11-15 13:23:53 +0100

    linked list solution

--- problems/python/linked_list.py
+++ problems/python/linked_list.py
@@ -2,7 +2,7 @@ from typing import List
 
 class Node:
     def __init__(self, val=0, next=None):
-        self.val = val
+        self.value = val
         self.next = next
     
 class LinkedList:
@@ -11,41 +11,94 @@ class LinkedList:
         self.end = None
     
     def get(self, index: int) -> int:
-        current_node = self.head
-        for i in range(0, index):
+        current = self.head
+        i = 0
+        while current:        
             if index == i:
-                return current_node.value
+                return current.value
             else:
-                current_node = current_node.next
-                
+                current = current.next
+            i += 1
+
         return -1    
-        
+         
     def insertHead(self, val: int) -> None:
         if not self.head: 
             self.head = Node(val)
-            self.head.next = self.end            
+            self.end = self.head
         else:
             new_head = Node(val, self.head)
             self.head = new_head
+ 
         
     def insertTail(self, val: int) -> None:
         if not self.end:
             self.end = Node(val)
-            self.head.next = self.end
+            self.head = self.end
         else:
             new_end = Node(val)
             self.end.next = new_end
-            self.end = new_end
+            self.end = self.end.next
 
     
-        
     def remove(self, index: int) -> bool:        
+        if not self.head:
+            return False
+        
+        if index == 0:
+            self.head = self.head.next
+            return True
+        
+        prev = None
+        current = self.head
+        i = 0
+
+        while current:            
+            if index == i:
+                if current == self.end:
+                    self.end = prev
+                    self.end.next = None
+                else:
+                    prev.next = current.next                
+                return True                
+            else:                    
+                prev = current
+                current = current.next
+
+            i += 1
         return False
 
     def getValues(self) -> List[int]:
-        return []
+        values = []
+        current = self.head
+
+        while current:
+            values.append(current.value)
+            current = current.next
+
+        return values
+    
+    def printList(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
 
 ll = LinkedList()
 
+ll.insertTail(1)
+ll.insertTail(2)
+print(ll.get(1))
+print(ll.remove(1))
 
-ll.insertHead()
+ll.insertTail(2)
+print(ll.getValues())
+print(ll.get(1))
+print(ll.get(0))
+
+print(ll.getValues())
+
+
+
+
+
