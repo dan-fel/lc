df81cb1ea5bdbf3a6957bcff66558fd0f7780515
danfel <daniel.felczak@gmail.com>
2023-12-08 20:26:14 +0100

    Some solutions

---
+++ problems/python/36_valid_sudoku.py
@@ -0,0 +1,72 @@
+from typing import List
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        """
+        So we have a matrix, this means we should index i, j
+            we want to hash by the index, so 
+                for row i, there shall be a set which we will fill.
+                for column j, there shall also be a set which we will also fill
+                for the square itself, it will be computed while we do the rows and cols                            
+        """
+
+        # 1 Start with the rows.
+
+        rows = 8
+        cols = 8
+
+        row_set = set()
+        col_set = set()
+
+        square_map = {}
+
+        for i in range(0, rows):
+            row_set.clear()
+            col_set.clear()
+            for j in range(0, cols):
+                
+                row_sq_idx = (i + 3) // 3
+                col_sq_idx = (j + 3) // 3
+
+                if not square_map[(row_sq_idx, col_sq_idx)]:
+                    square_map[(row_sq_idx, col_sq_idx)] = set()
+                else:
+                    square_map[(row_sq_idx, col_sq_idx)].add(board[i][j])
+
+                row_val = board[i][j]
+                col_val = board[j][i]
+                
+                 
+                if row_val in row_set or col_val in col_set:
+                    print(row_val, row_set)
+                    print(col_val, col_set)
+                    return False
+                else:
+                    if col_val != '.':
+                        col_set.add(col_val)
+                    if row_val != '.':
+                        row_set.add(row_val)
+
+        print(square_map)                    
+                
+        return True
+    
+        
+
+    
+            
+
+
+board = [["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+
+sol = Solution()
+
+print(sol.isValidSudoku(board))
