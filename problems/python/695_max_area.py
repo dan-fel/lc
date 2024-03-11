4f440017b406108adede5503052a3ce482a2077e
danfel <daniel.felczak@gmail.com>
2024-03-11 17:24:35 +0100

    695 sol

--- problems/python/695_max_area.py
+++ problems/python/695_max_area.py
@@ -14,19 +14,22 @@ class Solution:
         for i in range(0, len(grid)):
             for j in range(0, len(grid[0])):
                 area = 0
-                if grid[i][j] == 1:
+                if grid[i][j] == 1:                    
                     queue.append((i,j))
-                while queue:
-                    print(area)
+                    visited.add((i,j))
+                while queue:                    
                     area += 1
-                    max_area = max(max_area, area)
+                    max_area = max(max_area, area)                
                     t = queue.popleft()
                     for d in dirs:
-                        if grid[t[0] + d[0]][t[1] + d[1]] == 1:
-                            queue.append((t[0] + d[0], t[1] + d[1]))
-                            visited.add((t[0] + d[0], t[1] + d[1]))
+                        if t[0] + d[0] >= 0 and t[0] + d[0] < len(grid) and t[1] + d[1] >= 0 and t[1] + d[1] < len(grid[0]):
+                            if grid[t[0] + d[0]][t[1] + d[1]] == 1 and (t[0] + d[0], t[1] + d[1]) not in visited:                                                                
+                                queue.append((t[0] + d[0], t[1] + d[1]))
+                                visited.add((t[0] + d[0], t[1] + d[1]))
         
         return max_area
 
 grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+g2 = [[1]]
 print(Solution().maxAreaOfIsland(grid))
+print(Solution().maxAreaOfIsland(g2))
